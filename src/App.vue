622fbc246b56f6fe549e99b90768aa8171305287
Pietromuresu <muresupietro66@gmail.com>
2023-05-04 16:34:49 +0200

    Milestone 0

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script>
+import axios from '../node_modules/axios';
+
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+import Main from './components/Main.vue'
+import Search from './components/Search.vue'
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Footer,
+    Main,
+    Search
+  },
+  data(){
+    return{
+      axios
+    }
+  }
+}
+</script>
+
+<template>
+  <Header/>
+  <Search/>
+  <Main/>
+  <Footer/>
+</template>
+
+<style lang="scss">
+@import './scss/main.scss';
+@import 'bootstrap/scss/bootstrap';
+
+
+</style>
---
+++ src/components/partials/Loader.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: 'Loader', 
+}
+</script>
+
+<template>
+  
+</template>
+
+<style lang="scss" scoped>
+
+</style>
